1b9a73d8c3e3aa5b569bccaf97d81bd34bc0fd2d
MrXia <1452334161@qq.com>
2016-10-31 21:48:57 +0800

    Initial Commit

---
+++ DatePickerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatePickerExample
+//
+//  Created by MrXia on 16/10/31.
+//  Copyright © 2016年 MrXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
